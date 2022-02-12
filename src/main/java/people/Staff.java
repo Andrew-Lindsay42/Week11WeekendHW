5531e66f43a944bac98c51b641edb4313023e537
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 15:23:01 +0000

    Created Staff abstract Class

--- src/main/java/people/Staff.java
+++ src/main/java/people/Staff.java
@@ -1,4 +1,15 @@
 package people;
 
-public class Staff {
+public abstract class Staff extends Person{
+    private Rank rank;
+
+    public Staff(String name, Rank rank) {
+        super(name);
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
 }

7e624febb3d60d7650c21ee0ceabe72672850fd3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 15:24:09 +0000

    Created Pilot Class - passes tests

--- src/main/java/people/Pilot.java
+++ src/main/java/people/Pilot.java
@@ -1,4 +1,14 @@
 package people;
 
-public class Pilot {
+public class Pilot extends Staff{
+    private String licenceNumber;
+
+    public Pilot(String name, Rank rank, String licenceNumber) {
+        super(name, rank);
+        this.licenceNumber = licenceNumber;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
 }

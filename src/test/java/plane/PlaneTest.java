95edb22461921d5b5c038596798d3a90306fb359
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 16:40:49 +0000

    Added tests for Plane Class

--- src/test/java/plane/PlaneTest.java
+++ src/test/java/plane/PlaneTest.java
@@ -1,4 +1,31 @@
 package plane;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals(PlaneType.BOEING747, plane.getType());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(524, plane.getCapacity());
+    }
+
+    @Test
+    public void hasWeight(){
+        assertEquals(183500, plane.getWeight());
+    }
+
 }

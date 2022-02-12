79b62cdc512fe5466e8b58eca66bfdcbd9d1825f
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 15:20:17 +0000

    Created tests of Pilot

--- src/test/java/people/PilotTest.java
+++ src/test/java/people/PilotTest.java
@@ -1,4 +1,30 @@
 package people;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Greg", Rank.CAPTAIN, "ABC123");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Greg", pilot.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void hasLicenceNumber(){
+        assertEquals("ABC123", pilot.getLicenceNumber());
+    }
 }

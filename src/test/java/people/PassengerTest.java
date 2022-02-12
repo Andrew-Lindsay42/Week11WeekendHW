d628cdb4169d22722d9f79de1f12739f6e3ad148
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 12:40:24 +0000

    Added tests for Passenger Class

--- src/test/java/people/PassengerTest.java
+++ src/test/java/people/PassengerTest.java
@@ -1,4 +1,26 @@
 package people;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class PassengerTest {
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Andrew", 2);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Andrew", passenger.getName());
+    }
+
+    @Test
+    public void hasBags(){
+        assertEquals(2, passenger.getBags());
+    }
 }

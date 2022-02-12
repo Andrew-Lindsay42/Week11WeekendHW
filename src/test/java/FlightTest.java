482ab079ef1512301ad97842c517388e8c372073
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 20:56:09 +0000

    Created Tests for Flight Class

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,69 @@
+import org.junit.Before;
+import org.junit.Test;
+import people.CabinCrew;
+import people.Passenger;
+import people.Pilot;
+import people.Rank;
+import plane.Plane;
+import plane.PlaneType;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    Flight flight;
+    Pilot pilot;
+    ArrayList<CabinCrew> cabinCrew;
+    Plane plane;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Greg", Rank.CAPTAIN, "ABC123");
+        cabinCrew = new ArrayList<>();
+        cabinCrew.add(new CabinCrew("Tim", Rank.CABIN_CREW));
+        cabinCrew.add(new CabinCrew("Rebecca", Rank.PURSER));
+        plane = new Plane(PlaneType.CESSNA_172);
+        flight = new Flight(pilot, cabinCrew, plane, "ZT141", "FRA", "EDI", "12:30");
+    }
+
+    @Test
+    public void hasPilot(){
+        assertEquals(pilot, flight.getPilot());
+    }
+
+    @Test
+    public void hasCabinCrew(){
+        assertEquals(cabinCrew, flight.getCabinCrew());
+    }
+
+    @Test
+    public void hasPlane(){
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    public void hasFlightNumber(){
+        assertEquals("ZT141", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("FRA", flight.getDestination());
+    }
+
+    @Test
+    public void hasDepartureAirport(){
+        assertEquals("EDI", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void hasDepartureTime(){
+        assertEquals("12:30", flight.getDepartureTime());
+    }
+
+    @Test
+    public void passengersStartAtZero(){
+        assertEquals(0, flight.countPassengers());
+    }
 }

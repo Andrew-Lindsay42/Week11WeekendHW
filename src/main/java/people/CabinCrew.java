2c36854643d5194d59402b6066b43a9b09869998
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 16:17:36 +0000

    Added CabinCrew Class

--- src/main/java/people/CabinCrew.java
+++ src/main/java/people/CabinCrew.java
@@ -1,4 +1,12 @@
 package people;
 
-public class CabinCrew {
+public class CabinCrew extends Staff{
+
+    public CabinCrew(String name, Rank rank) {
+        super(name, rank);
+    }
+
+    public String giveBriefing(){
+        return "The emergency exits are located .... etc";
+    }
 }

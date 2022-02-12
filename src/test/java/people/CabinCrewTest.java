2a386e93c1d6ffad259441dc7ef5957be0578d62
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-12 16:17:13 +0000

    Created tests for CabinCrew

--- src/test/java/people/CabinCrewTest.java
+++ src/test/java/people/CabinCrewTest.java
@@ -1,4 +1,30 @@
 package people;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewTest {
+    CabinCrew cabinCrew;
+
+    @Before
+    public void before(){
+        cabinCrew = new CabinCrew("Tim", Rank.CABIN_CREW);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Tim", cabinCrew.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.CABIN_CREW, cabinCrew.getRank());
+    }
+
+    @Test
+    public void canGiveBriefing(){
+        assertEquals("The emergency exits are located .... etc", cabinCrew.giveBriefing());
+    }
 }
